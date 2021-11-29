519f226c492964b05ccebe9136d420f648bb375e
msr <mshineray@qq.com>
2021-11-29 14:21:13 +0800

    upd:  js 动态插入脚本

---
+++ src/loadScript.js
@@ -0,0 +1,19 @@
+/**
+ *  js 动态插入脚本
+ * @param url
+ * @param callback
+ */
+function loadScript(url, callback) {
+  callback = typeof callback === 'function' ? callback : function() {};
+  var head = document.getElementsByTagName('head')[0];
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
+  script.onreadystatechange = function() {
+    if(this.readyState === "loaded" || this.readyState === "complete"){
+      callback();
+    }
+  }
+  script.onload = callback;
+  head.appendChild(script);
+}
