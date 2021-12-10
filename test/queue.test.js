c104908be514cfb5be9474bd4d46d006b319bcc7
msr <mshineray@qq.com>
2021-12-10 17:20:01 +0800

    upd: queue

---
+++ test/queue.test.js
@@ -0,0 +1,14 @@
+import Queue from '../src/queue';
+const queue = new Queue()
+
+test('queue.isEmpty() === true', () => {
+  expect(queue.isEmpty()).toBe(true);
+});
+
+test('queue.enqueue().toString()', () => {
+
+  queue.enqueue(5)
+  queue.enqueue(8)
+
+  expect(queue.toString()).toBe("5, 8");
+});
