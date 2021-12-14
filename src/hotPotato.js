14b06f7aac88fb66e6150f90155b6d2de235b321
msr <mshineray@qq.com>
2021-12-14 19:12:30 +0800

    upd: deque

---
+++ src/hotPotato.js
@@ -0,0 +1,20 @@
+import Queue from './queue'
+function hotPotato(elementsList, num){
+  const queue = new Queue()
+  const elimitatedList = []
+  for(let i=0; i<elementsList.length; i++){
+    queue.enqueue(elementsList[i])
+  }
+
+  while (queue.size() > 1){
+    for (let i = 0; i < num; i++){
+      queue.enqueue(queue.dequeue())
+    }
+    elimitatedList.push(queue.dequeue())
+  }
+  return {
+    elimited: elimitatedList,
+    winner: queue.dequeue()
+  }
+}
+export default hotPotato
---
+++ test/hotPotato.test.js
@@ -0,0 +1,11 @@
+import hotPotato from '../src/hotPotato';
+
+test('test', () => {
+  const names = ['John', 'Jack', 'Camila', 'Ingrid', 'Carl']
+  const result = hotPotato(names, 7)
+  result.elimited.forEach(name => {
+    console.log(`${name} 在击鼓传花游戏中淘汰。`)
+  })
+  console.log(`胜利者：${result.winner}`)
+
+});
