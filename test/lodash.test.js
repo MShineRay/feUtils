fd674c89842911403f36fe635a834b0bfad7ff7f
msr <mshineray@qq.com>
2022-02-15 15:41:00 +0800

    upd: test

--- test/lodash.test.js
+++ test/lodash.test.js
@@ -3,12 +3,12 @@ import {add, multiply} from 'lodash'
 // lodash 数学计算方法有精度误差
 
 describe('lodash test', ()=>{
-  test('lodash add 1 + 2  to equal 3', () => {
+  it('lodash add 1 + 2  to equal 3', () => {
     expect(add(1, 2)).toBe(3)
   })
 
 // 155.70*100
-  test('lodash multiply 155.70 * 100 not to equal 15570', () => {
+  it('lodash multiply 155.70 * 100 not to equal 15570', () => {
     expect(multiply(155.70, 100)).toBeLessThan(15570)
     // Expected: 15570
     // Received: 15569.999999999998
--- test/navigator.test.js
+++ test/navigator.test.js
@@ -1,79 +1,81 @@
 import { getUAInfo } from '../src/navigator'
-// TODO：参考platform.js 测试集
-// https://github.com/bestiejs/platform.js/blob/master/test/test.js
-const uaList = [
-  {
-    ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36',
-    browserName: 'Chrome',
-    browserVersion: '98.0.4758.80',
-    osName: 'Mac',
-    osVersion: '10_15_7'
-  },
-  {
-    ua: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0",
-    browserName: 'Firefox',
-    browserVersion: '97.0',
-    osName: 'Mac',
-    osVersion: '10.15'
-  },
-  {
-    ua: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15",
-    browserName: 'Safari',
-    browserVersion: '14.1.1',
-    osName: 'Mac',
-    osVersion: '10_15_7'
-  },
-  {
-    ua: "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
-    browserName: 'Safari',
-    browserVersion: '5.1',
-    osName: 'Windows',
-    osVersion: '6.1'
-  },
-  {
-    ua: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
-    browserName: 'Chrome',
-    browserVersion: '14.0.835.163',
-    osName: 'Windows',
-    osVersion: '6.1'
-  },
-  {
-    ua: 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
-    browserName: 'Firefox',
-    browserVersion: '6.0',
-    osName: 'Windows',
-    osVersion: '6.1'
-  },
-  {
-    ua: 'Opera/9.80 (Windows NT 6.1; U; zh-cn) Presto/2.9.168 Version/11.50',
-    browserName: 'Opera',
-    browserVersion: '11.50',
-    osName: 'Windows',
-    osVersion: '6.1'
-  },
-  {
-    ua: 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; Tablet PC 2.0; .NET4.0E)',
-    browserName: 'IE',
-    browserVersion: '9.0',
-    osName: 'Windows',
-    osVersion: '6.1'
-  },
-  {
-    ua:"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; InfoPath.3)",
-    browserName: 'IE',
-    browserVersion: '8.0',
-    osName: 'Windows',
-    osVersion: '6.1'
-  }
-]
 
-test('ua', () => {
-  for (let i=0, len=uaList.length; i<len; i++){
-    let item = uaList[i]
-    let uaInfo = getUAInfo(item.ua)
-    expect(uaInfo.osName).toBe(item.osName)
-    expect(uaInfo.osVersion).toBe(item.osVersion)
-    expect(uaInfo.browserName).toBe(item.browserName)
-    expect(uaInfo.browserVersion).toBe(item.browserVersion)
-  }
+describe('navigator.userAgent', ()=>{
+  it('ua', () => {
+    // TODO：参考platform.js 测试集
+    // https://github.com/bestiejs/platform.js/blob/master/test/test.js
+    const uaList = [
+      {
+        ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36',
+        browserName: 'Chrome',
+        browserVersion: '98.0.4758.80',
+        osName: 'Mac',
+        osVersion: '10_15_7'
+      },
+      {
+        ua: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0",
+        browserName: 'Firefox',
+        browserVersion: '97.0',
+        osName: 'Mac',
+        osVersion: '10.15'
+      },
+      {
+        ua: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15",
+        browserName: 'Safari',
+        browserVersion: '14.1.1',
+        osName: 'Mac',
+        osVersion: '10_15_7'
+      },
+      {
+        ua: "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+        browserName: 'Safari',
+        browserVersion: '5.1',
+        osName: 'Windows',
+        osVersion: '6.1'
+      },
+      {
+        ua: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
+        browserName: 'Chrome',
+        browserVersion: '14.0.835.163',
+        osName: 'Windows',
+        osVersion: '6.1'
+      },
+      {
+        ua: 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
+        browserName: 'Firefox',
+        browserVersion: '6.0',
+        osName: 'Windows',
+        osVersion: '6.1'
+      },
+      {
+        ua: 'Opera/9.80 (Windows NT 6.1; U; zh-cn) Presto/2.9.168 Version/11.50',
+        browserName: 'Opera',
+        browserVersion: '11.50',
+        osName: 'Windows',
+        osVersion: '6.1'
+      },
+      {
+        ua: 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; Tablet PC 2.0; .NET4.0E)',
+        browserName: 'IE',
+        browserVersion: '9.0',
+        osName: 'Windows',
+        osVersion: '6.1'
+      },
+      {
+        ua:"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; InfoPath.3)",
+        browserName: 'IE',
+        browserVersion: '8.0',
+        osName: 'Windows',
+        osVersion: '6.1'
+      }
+    ]
+    for (let i=0, len=uaList.length; i<len; i++){
+      let item = uaList[i]
+      let uaInfo = getUAInfo(item.ua)
+      expect(uaInfo.osName).toBe(item.osName)
+      expect(uaInfo.osVersion).toBe(item.osVersion)
+      expect(uaInfo.browserName).toBe(item.browserName)
+      expect(uaInfo.browserVersion).toBe(item.browserVersion)
+    }
+  })
 })
