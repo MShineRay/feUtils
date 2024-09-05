e00d4e7b8cfa58d9060919f9258d8d8a6a051b0c
msr <mshineray@qq.com>
2024-09-05 16:23:56 +0800

    export

--- src/index.js
+++ src/index.js
@@ -10,3 +10,17 @@ export {loadScript} from './loadScript'
 export {mathSubtract, mathAdd, mathMultiply, mathDivide, mathToFixed} from './math'
 
 export {maskStr} from './string'
+export {isLoadedScript} from './loadScript'
+
+export default {
+  generateDeviceId,
+  lunhCheck,
+  loadScript,
+  mathSubtract,
+  mathAdd,
+  mathMultiply,
+  mathDivide,
+  mathToFixed,
+  maskStr,
+  isLoadedScript
+}
