28615f82bd05af3cb861f0ffa4ec6b6c9f737de3
msr <mshineray@qq.com>
2024-10-04 21:38:25 +0800

    feat: upd

--- src/copyText.js
+++ src/copyText.js
@@ -5,20 +5,20 @@
 export function copyText (text) {
   if(navigator.clipboard){
     // copyText = (text) =>{// 惰性函数方式
-      navigator.clipboard.writeText(text).then(function() {
-        console.log('Async: Copying to clipboard was successful!')
-      }, function(err) {
-        console.error('Async: Could not copy text: ', err)
-      })
+    navigator.clipboard.writeText(text).then(function() {
+      console.log('Async: Copying to clipboard was successful!')
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err)
+    })
     // }
   }else{
     // copyText = (text) =>{// 惰性函数方式
-      const textArea = document.createElement('textarea')
-      textArea.value = text
-      document.body.appendChild(textArea)
-      textArea.select()
-      document.execCommand('copy')
-      document.body.removeChild(textArea)
+    const textArea = document.createElement('textarea')
+    textArea.value = text
+    document.body.appendChild(textArea)
+    textArea.select()
+    document.execCommand('copy')
+    document.body.removeChild(textArea)
     // }
   }
 }
